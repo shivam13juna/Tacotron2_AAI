77f3727a309f0db5ab3ec79b11333a45e4370b3d
Shivam Prasad <shivam13juna@gmail.com>
2019-02-13 10:44:56 +0530

    About to make changes in logger

--- train.py
+++ train.py
@@ -242,15 +242,13 @@ def train(output_directory, log_directory, checkpoint_path, warm_start, n_gpus,
                 logger.log_training(
                     reduced_loss, grad_norm, learning_rate, duration, iteration)
 
-            # if not overflow and (iteration % hparams.iters_per_checkpoint == 0):
-            #     validate(model, criterion, valset, iteration,
-            #              hparams.batch_size, n_gpus, logger,collate_fn,
-            #              hparams.distributed_run, rank)
-            #     if rank == 0:
-            #         checkpoint_path = os.path.join(
-            #             output_directory, "checkpoint_{}".format(iteration))
-            #         save_checkpoint(model, optimizer, learning_rate, iteration,
-            #                         checkpoint_path)
+            if not overflow and (iteration % hparams.iters_per_checkpoint == 0):
+                validate(model, criterion, valset, iteration,
+                         hparams.batch_size, n_gpus, logger,collate_fn,
+                         hparams.distributed_run, rank)
+                if rank == 0:
+                    checkpoint_path = os.path.join(output_directory, "checkpoint_{}".format(iteration))
+                    save_checkpoint(model, optimizer, learning_rate, iteration, checkpoint_path)
 
             iteration += 1
 
