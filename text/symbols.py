106804d9e2d617ddf549bb5b841f5800ad488c8d
Shivam Prasad <shivam13juna@gmail.com>
2019-02-28 12:07:14 +0530

    increasing to 600 dimension didn't work as well

--- text/symbols.py
+++ text/symbols.py
@@ -5,6 +5,7 @@ Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
 from text import cmudict
+import pickle
 
 _pad        = '_'
 _punctuation = '!\'(),.:;? '
@@ -15,4 +16,9 @@ _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+# symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+with open('variables/set_phoneme', 'rb') as handle: 
+    sett = pickle.loads(handle.read())
+
+symbols = list(sett)
+
